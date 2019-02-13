6d6e49d5539cb825d8faed658704b130a0f00168
Adhya <50034618@acsind.com>
2019-02-13 11:35:10 +0530

    initial commit

---
+++ Sleep.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Threading.Sleep
+{
+    public class FirstThread
+    {
+        public void CreateFirstThread()
+        {
+            Thread FirstThread = new Thread(FirstFunction);
+            FirstThread.Name = "First Thread";
+            Console.WriteLine("Threading Starts...");
+            FirstThread.Start();
+
+
+            Console.WriteLine("Thread Properties...");
+            ThreadProperties(FirstThread);
+        }
+
+        private void ThreadProperties(Thread firstThread)
+        {
+            Console.WriteLine("ThreadName: " + firstThread.Name);
+            Console.WriteLine("CurrentCulture: " + firstThread.CurrentCulture);
+            Console.WriteLine("CurrentUICulture: " + firstThread.CurrentUICulture);
+            Console.WriteLine("ExecutionContext: " + firstThread.ExecutionContext);
+            Console.WriteLine("IsAlive: " + firstThread.IsAlive);
+            Console.WriteLine("IsBackground: " + firstThread.IsBackground);
+            Console.WriteLine("IsThreadPoolThread: " + firstThread.IsThreadPoolThread);
+            Console.WriteLine("ManagedThreadId: " + firstThread.ManagedThreadId);
+            Console.WriteLine("Priority: " + firstThread.Priority);
+            Console.WriteLine("ThreadState: " + firstThread.ThreadState);
+
+        }
+
+        public void FirstFunction()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("First thread Displays: " + i);
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("\nPress enter to quit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Suspend.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading.Suspend
+{
+    public class FirstThread
+    {
+        public void CreateFirstThread()
+        {
+            Thread firstThread = new Thread(FirstFunction);
+            firstThread.Name = "First Thread";
+            Console.WriteLine("Threading Starts...");
+            firstThread.Start();
+            firstThread.
+
+            Console.WriteLine("Thread Properties...");
+            ThreadProperties(firstThread);
+        }
+
+        private void ThreadProperties(Thread firstThread)
+        {
+            Console.WriteLine("ThreadName: " + firstThread.Name);
+            Console.WriteLine("CurrentCulture: " + firstThread.CurrentCulture);
+            Console.WriteLine("CurrentUICulture: " + firstThread.CurrentUICulture);
+            Console.WriteLine("ExecutionContext: " + firstThread.ExecutionContext);
+            Console.WriteLine("IsAlive: " + firstThread.IsAlive);
+            Console.WriteLine("IsBackground: " + firstThread.IsBackground);
+            Console.WriteLine("IsThreadPoolThread: " + firstThread.IsThreadPoolThread);
+            Console.WriteLine("ManagedThreadId: " + firstThread.ManagedThreadId);
+            Console.WriteLine("Priority: " + firstThread.Priority);
+            Console.WriteLine("ThreadState: " + firstThread.ThreadState);
+
+        }
+
+        public void FirstFunction()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                int time = new Random().Next(25, 50);
+                Console.WriteLine("FirstThread Slept for : {0} Milliseconds", time.ToString());
+                Console.WriteLine("First thread Displays: " + i);
+                Thread.Sleep(time);
+            }
+            Console.WriteLine("\nPress enter to quit...");
+            Console.ReadLine();
+        }
+    }
+}
